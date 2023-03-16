0614aca4991a26ea95e6afd3e0fb07104d22db48
sbrc1996 <sbrc1996@gmail.com>
2023-03-17 01:07:06 +0530

    Sixth Commit with Log4j Files in the master branch...

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,26 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
 public class Main {
+
+    private static Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args)
     {
+        logger.info("\n Start of Logging...");
+        logger.error("\n This is an test error message...");
+        logger.fatal("\n This is an test fatal massage...");
+
         System.out.println("Welcome to Mini Calculator!!");
+
         Calculator cc = new Calculator();
         do{
+            logger.debug("\n Control now in the main loop....");
+
             System.out.println("Enter 1. For Addition!");
             System.out.println("Enter 2. For Subtraction!");
             System.out.println("Enter 3. For Multiply!");
@@ -21,6 +34,8 @@ public class Main {
             switch(choice)
             {
                 case 1:
+                    logger.debug("\n Control with the addition function....");
+
                     System.out.println("Enter the first number");
                     first = sc.nextInt();
                     System.out.println("Enter the second number");
@@ -28,6 +43,8 @@ public class Main {
                     System.out.println("The answer for addition of "+first+" and "+second+" is "+cc.add(first,second));
                     break;
                 case 2:
+                    logger.debug("\n Control with the Subtraction function....");
+
                     System.out.println("Enter the first number");
                     first = sc.nextInt();
                     System.out.println("Enter the second number");
@@ -35,6 +52,8 @@ public class Main {
                     System.out.println("The answer for subtraction of "+first+" and "+second+" is "+cc.sub(first,second));
                     break;
                 case 3:
+                    logger.debug("\n Control with the Multiplication function....");
+
                     System.out.println("Enter the first number");
                     first = sc.nextInt();
                     System.out.println("Enter the second number");
@@ -42,11 +61,16 @@ public class Main {
                     System.out.println("The answer for multiplication of "+first+" and "+second+" is "+cc.mul(first,second));
                     break;
                 case 4:
+                    logger.debug("\n See you later...");
+
                     System.out.println("Thanks for visiting, see you soon!!!");
                     System.exit(0);
                 default:
+                      logger.debug("\n Control with the default case....");
+
                       System.out.println("Wrong Input, Try again!!");
             }
         }while(true);
+
     }
 }
